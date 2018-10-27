4c37fb92d4d30334f670693cdf5219bfaccd1365
sean shan <adls371@outlook.com>
2018-10-27 16:12:50 -0400

    handle ignore line

--- convert.sh
+++ convert.sh
@@ -4,11 +4,12 @@
 EASYCCG_HOME="./easyccg"
 input_file="QALD-questions.txt"
 to_tree_command="python to_tree.py"
+ignore_line=1
 
 while getopts "hi:" o; do
 	case $o in
 		i)
-			ignore_line=${OPTARG}
+			ignore_line=`expr ${OPTARG} + 1`
 			;;
 		h)
 			echo "usage: ./convert [-i 1]"
@@ -21,11 +22,11 @@ while getopts "hi:" o; do
 	esac
 done
 
-tree_string=`cat ${input_file} | java -jar ${EASYCCG_HOME}/easyccg.jar --model ${EASYCCG_HOME}/model_questions -s -r S[q] S[qem] S[wq]`
+tree_string=`cat ${input_file}| cut -d' ' -f${ignore_line}- | java -jar ${EASYCCG_HOME}/easyccg.jar --model ${EASYCCG_HOME}/model_questions -s -r S[q] S[qem] S[wq]`
 
 echo "${tree_string}"| while read id_line;read parse_line
 do
 	tree=`echo "$parse_line" | eval ${to_tree_command}`
 	line_num=`echo $id_line | cut -d'=' -f2`
-	printf "%s, %s:\n%s\n" $line_num "$tree"
+	printf "%s:\n%s\n" $line_num "$tree"
 done
