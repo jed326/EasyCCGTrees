6a8746eae6b0f06cf91c9d446cf1052dbb2e568f
stevenfontanella <steven.fontanella@gmail.com>
2018-10-27 14:21:37 -0400

    Commit for Jay

--- groupLines.py
+++ groupLines.py
@@ -1,8 +1,10 @@
 import subprocess
 import pathlib
 import collections
+import argparse
 from EasyCCGTrees.to_tree import to_tree, print_tree
 # from EasyCCGTrees.Visualize import build
+import itertools
 
 EASYCCG_HOME = pathlib.Path("/opt/easyccg/")
 OUT_PATH = pathlib.Path("/tmp/")
@@ -20,7 +22,7 @@ def remove_IDs():
                 if count % 2 == 1:
                     print(line.rstrip(), file = newfile)
 
-def tree_equals(tree1, tree2):
+def tree_equals(tree1, tree2, tolerance = 0):
     if not tree1.children and not tree2.children:
         return True
     elif len(tree1.children) != len(tree2.children):
@@ -35,13 +37,36 @@ def label(text):
     proc = subprocess.run(["java", "-jar", str(EASYCCG_HOME/"easyccg.jar"), "-f", "/tmp/_labeltmp",
         "--model", str(EASYCCG_HOME/"model_questions")], capture_output = True)
 
-    return str(proc.stdout.rstrip())
+    return proc.stdout.decode("utf-8").split("\n")[1].rstrip()
 
-def group(file):
-    with open(file) as input_file:
-        pass
+def group(file_path):
+    #list of list of trees
+    categories = []
 
-if __name__ == "__main__":
+    with open(file_path) as input_file:
+        labelled = label(input_file.read()).split("\n")[1::2]
+
+    labelled = labelled[:20]
+    trees = list(map(labelled, to_tree))
+    #print("\n".join(labelled[:10]))
+
+    for parse in labelled:
+        for category in categories:
+            if tree_equals(to_tree(parse), category[0]):
+                category.append(parse)
+                break
+        else:
+            categories.append([parse])
+    with open("_grouped_out.txt", "w") as grouped_file:
+        for category in categories:
+            grouped_file.write("\n".join(str(parse) for parse in category))
+            grouped_file.write("\n")
+
+    
+
+    # for question in labelled.split("\n"):
+
+def _test():
     run_easyCCG("QALD-questions.txt-stripped.txt")
     remove_IDs()
     tree = None
@@ -56,8 +81,22 @@ if __name__ == "__main__":
 
     # print(label("Which presidents were born in 1945?"))
     test1 = to_tree(label("What presidents were born in 1945"))
-    test2 = to_tree(label("Which presidents were born in 1945"))
+    test2 = to_tree(label("Which presidents were born in 1946"))
     test3 = to_tree(label("Which presidents were not born in 1945"))
+
+    test4 = to_tree(label("How tall is John Windsor"))
+    test5 = to_tree(label("How tall is Kate Upton"))
+
+    print(label("How tall is John Windsor"))
+    print_tree(test4)
+
+    print_tree(test1)
 
     print(tree_equals(test1,test2))
     print(tree_equals(test1,test3))
+    print(tree_equals(test4,test5))
+
+
+if __name__ == "__main__":
+    _test()
+#    group("QALD-questions.txt-stripped.txt")
