3e3385b89ab39d4eeb14ce9b8d21e7368aaaa5a3
v1siuol <lvzh.louis2013@gmail.com>
2018-10-27 15:19:50 -0400

    will ignore output.txt

--- convert.sh
+++ convert.sh
@@ -4,7 +4,7 @@
 EASYCCG_HOME="./easyccg"
 input_file="QALD-questions.txt"
 output_file="output.txt"
-to_tree_command="python3.7 to_tree.py"
+to_tree_command="python3 to_tree.py"
 
 cat ${input_file}| while read line; do
 	line_num=`echo $line | cut -d' ' -f1`
