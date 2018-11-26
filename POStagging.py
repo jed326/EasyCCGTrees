7c28aad604bae6aa281b4b8cd102b3d769dd7d38
stevenfontanella <steven.fontanella@gmail.com>
2018-11-26 16:14:52 -0500

    Add to_tags, speed up by loading only once

--- POStagging.py
+++ POStagging.py
@@ -4,6 +4,7 @@
 
 import spacy
 
+nlp = spacy.load('en_core_web_sm')
 def postagger(nlquestion):
     '''
     Input: Natural language sentence
@@ -11,15 +12,14 @@ def postagger(nlquestion):
     where a is a word in the sentence
     and b is the coarse-grained part-of-speech tag of the word.
     '''
-    nlp = spacy.load('en_core_web_sm')
     doc = nlp(nlquestion)
-    
+
     ans = []
     for token in doc:
         ans.append((token.text, token.pos_))
-    
+
     return ans
-    
+
 def postagger2(nlquestion):
     '''
     Input: Natural language sentence
@@ -27,18 +27,22 @@ def postagger2(nlquestion):
     where a is a word in the sentence
     and b is the fine-grained part-of-speech tag of the word.
     '''
-    nlp = spacy.load('en_core_web_sm')
     doc = nlp(nlquestion)
-    
+
     ans = []
     for token in doc:
         ans.append((token.text, token.tag_))
-    
+
     return ans
+
+def to_tags(question):
+    return " ".join(map(lambda x: x[1], postagger(question)))
 
 def _test():
     print(postagger("The quick brown fox jumps over the lazy dog"))
     print(postagger2("The quick brown fox jumps over the lazy dog"))
+
+    print(to_tags("The quick brown fox jumps over the lazy dog"))
 
 if __name__ == "__main__":
     _test()
